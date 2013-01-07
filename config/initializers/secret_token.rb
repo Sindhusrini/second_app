838b863719b6b01774e46383d5e4a7f14408363d
Sindhu Srinivasan <srinivasan.sindhu1@gmail.com>
2013-01-07 17:19:53 -0500

    Edited the versions in the gem file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '2710446a5aed0787b99276d7f46c84c22def48bfa6b799febaaf531a439902edd8c4fa5ae74bce7ad1be7d96fde255b02d9035dc9f22e3704096f18f7e818106'
